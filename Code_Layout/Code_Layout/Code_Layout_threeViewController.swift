323a371fd671160ca6e15ad8a5740ccfcc98d95f
Your Name <you@example.com>
2020-03-06 10:42:39 +0700

    two up

---
+++ Code_Layout/Code_Layout/Code_Layout_threeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Code_Layout_threeViewController.swift
+//  Code_Layout
+//
+//  Created by Ong_Lao_Ngao on 3/6/20.
+//  Copyright © 2020 Ong_Lao_Ngao. All rights reserved.
+//
+
+import UIKit
+
+class Code_Layout_threeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
