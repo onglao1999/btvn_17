ab013b551c8754c236819fa185a11b70dabfa220
Your Name <you@example.com>
2020-03-06 10:26:55 +0700

    btvn 17

---
+++ Code_Layout/Code_Layout/Code_Layout_two_ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  Code_Layout_two_ViewController.swift
+//  Code_Layout
+//
+//  Created by Ong_Lao_Ngao on 3/6/20.
+//  Copyright © 2020 Ong_Lao_Ngao. All rights reserved.
+//
+
+import UIKit
+
+class Code_Layout_two_ViewController: UIViewController {
+       let view1 = UIView()
+       let image_Trademark = UIImageView()
+       let phone = UITextField()
+       let password = UITextField()
+       let registration = UIButton()
+       let dctk = UILabel()
+       let login = UIButton()
+       let stackV = UIStackView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
